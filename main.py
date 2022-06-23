ccc8f2a9d1485fc9380cb0c79b21780d1c103854
Shapovalov Mihail <67328068+TheArcherST@users.noreply.github.com>
2022-06-23 19:10:07 +0300

    Add basic logging from aiogram

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import logging
 from aiogram.utils.executor import start_polling
 
 from bot import dp
+
+
+logging.basicConfig(level=logging.DEBUG, filename='debug.log')
 
 
 if __name__ == '__main__':
